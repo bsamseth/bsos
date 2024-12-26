b6ad78ec7f0068af4a69f94bdfa2d7029c475bfa
Bendik Samseth <bendik@samseth.me>
2024-12-26 15:31:34 +0100

    Add interrupt handling

--- src/lib.rs
+++ src/lib.rs
@@ -1,13 +1,19 @@
 #![no_std]
 #![cfg_attr(test, no_main)]
 #![feature(custom_test_frameworks)]
+#![feature(abi_x86_interrupt)]
 #![test_runner(crate::test_runner)]
 #![reexport_test_harness_main = "test_main"]
 #![allow(clippy::empty_loop)]
 
+mod interrupts;
 pub mod qemu;
 pub mod serial;
 pub mod vga;
+
+pub fn init() {
+    interrupts::init_idt();
+}
 
 // ================================================
 // Testing infrastructure:
@@ -48,6 +54,7 @@ pub fn test_panic_handler(info: &PanicInfo) -> ! {
 #[cfg(test)]
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
+    init();
     test_main();
     loop {}
 }
--- src/main.rs
+++ src/main.rs
@@ -13,9 +13,14 @@ use bsos::println;
 pub extern "C" fn _start() -> ! {
     println!("Hello World{}", "!");
 
+    bsos::init();
+
+    x86_64::instructions::interrupts::int3();
+
     #[cfg(test)]
     test_main();
 
+    println!("It did not crash!");
     loop {}
 }
 
